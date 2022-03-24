01b3896a5f35ea8b71ae62fd6c90038469e02697
josejesusgarciajr <josejesusgarciajr@gmail.com>
2022-03-23 18:27:22 -0700

    Deserialize Json to Object

---
+++ Cards/Cards/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Cards.Models;
+using System.Net;
+using System.IO;
+using System.Text.Json;
+
+namespace Cards.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /*
+         * https://docs.microsoft.com/en-us/dotnet/api/system.net.webrequest?view=net-6.0
+         */
+        public IActionResult ShuffleDeck()
+        {
+            WebRequest request =
+                WebRequest.Create("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            Console.WriteLine($"Response: {response}");
+            Console.WriteLine(response.StatusDescription);
+
+            // Get the stream containing content returned by the server.
+            Stream dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd();
+            // Display the content.
+            Console.WriteLine(responseFromServer);
+            // Cleanup the streams and the response.
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+
+            // json data
+            DeckOfCards? deckOfCards =
+                JsonSerializer.Deserialize<DeckOfCards>(responseFromServer);
+
+            Console.WriteLine("");
+            Console.WriteLine($"Success: {deckOfCards?.success}");
+            Console.WriteLine($"DeckID: {deckOfCards?.deck_id}");
+            Console.WriteLine($"Shuffled: {deckOfCards?.shuffled}");
+            Console.WriteLine($"Remaing: {deckOfCards?.remaining}");
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Cards/Cards/Models/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Cards.Models
+{
+    public class Card
+    {
+        public string Image { get; set; }
+        public string Value { get; set; }
+        public string Suit { get; set; }
+        public string Code { get; set; }
+
+        public Card(){ }
+
+        public Card(string image, string value, string suit, string code)
+        {
+            Image = image;
+            Value = value;
+            Suit = suit;
+            Code = code;
+        }
+
+
+    }
+}
